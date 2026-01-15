ba4ceee0b31f736469ecdf12b592d38fb4c778cc
ma11eo <matteo.novelli@itiscuneo.eu>
2026-01-15 11:50:23 +0100

    lezione 15/01/2026

--- AlphaBot.py
+++ AlphaBot.py
@@ -169,3 +169,173 @@ class AlphaBot(object):
   
 		self.speed[0] = max(0, min(100, self.speed[0]))
 		self.speed[1] = max(0, min(100, self.speed[1]))
+
+class ChillBot(object):
+	
+	# def __init__(self,in1=7,in2=16,ena=8,in3=14,in4=15,enb=18):
+	def __init__(self,in1=26,in2=36,ena=24,in3=8,in4=10,enb=12):
+		self.IN1 = in1
+		self.IN2 = in2
+		self.IN3 = in3
+		self.IN4 = in4
+		self.ENA = ena
+		self.ENB = enb
+  
+		self.moving = False
+		self.canMoveForward = True
+  
+		self.DL = 19
+		self.DR = 16
+  
+		self.speed = [50, 50]
+  
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.IN1,GPIO.OUT)
+		GPIO.setup(self.IN2,GPIO.OUT)
+		GPIO.setup(self.IN3,GPIO.OUT)
+		GPIO.setup(self.IN4,GPIO.OUT)
+		GPIO.setup(self.ENA,GPIO.OUT)
+		GPIO.setup(self.ENB,GPIO.OUT)
+  
+		# Sesonri Left e Right di luce
+		GPIO.setup(self.DL, GPIO.IN, GPIO.PUD_UP)
+		GPIO.setup(self.DR, GPIO.IN, GPIO.PUD_UP)
+  
+		# GPIO.input(DR)
+  
+		self.PWMA = GPIO.PWM(self.ENA,500)
+		self.PWMB = GPIO.PWM(self.ENB,500)
+		self.PWMA.start(50)
+		self.PWMB.start(50)
+		self.stop()
+  
+	def getSensors(self):
+		return (GPIO.input(self.DL),GPIO.input(self.DR)) 
+	
+	def stop(self):
+		print("im STOPPING")
+		GPIO.output(self.IN1,GPIO.LOW)
+		GPIO.output(self.IN2,GPIO.LOW)
+		GPIO.output(self.IN3,GPIO.LOW)
+		GPIO.output(self.IN4,GPIO.LOW)
+		self.moving = False
+
+	def forwardTime(self, seconds = 0):
+		if not self.canMoveForward:
+			self.stop()
+			return
+		print("im moving forward")
+		self.setMotor(-self.speed[0], self.speed[1])
+		time.sleep(seconds)
+		self.stop()
+	
+	def backwardTime(self, seconds = 0):
+		print("im moving forward")
+		self.setMotor(self.speed[0], -self.speed[1])
+		time.sleep(seconds)
+		self.stop()
+
+	def leftTime(self, seconds = 0):
+		print("im moving forward")
+		self.setMotor(-self.speed[0], -self.speed[1])
+		time.sleep(seconds)
+		self.stop()
+
+	def rightTime(self, seconds = 0):
+		print("im moving forward")
+		self.setMotor(self.speed[0], self.speed[1])
+		time.sleep(seconds)
+		self.stop()
+
+	def forward(self, seconds = 0):
+		if not self.canMoveForward:
+			self.stop()
+			return
+		
+		if self.moving:
+			return
+		
+		if self.getSensors()[0] == 0 or self.getSensors()[1] == 0:
+			return
+		
+		print("im moving forward")
+		# GPIO.output(self.IN1,GPIO.HIGH)
+		# GPIO.output(self.IN2,GPIO.LOW)
+		# GPIO.output(self.IN3,GPIO.LOW)
+		# GPIO.output(self.IN4,GPIO.HIGH)
+		self.setMotor(-self.speed[0], self.speed[1])
+		self.moving = True
+  		# time.sleep(seconds)
+		# self.stop()
+
+
+	def backward(self, seconds = 0):
+		if(self.moving):
+			return
+		print("im moving backward")
+		# GPIO.output(self.IN1,GPIO.LOW)
+		# GPIO.output(self.IN2,GPIO.HIGH)
+		# GPIO.output(self.IN3,GPIO.HIGH)
+		# GPIO.output(self.IN4,GPIO.LOW)
+		self.setMotor(self.speed[0], -self.speed[1])
+		self.moving = True
+		# time.sleep(seconds)
+		# self.stop()
+
+	def left(self, seconds = 0):
+		if(self.moving):
+			return
+		print("im moving left")
+		# GPIO.output(self.IN1,GPIO.LOW)
+		# GPIO.output(self.IN2,GPIO.LOW)
+		# GPIO.output(self.IN3,GPIO.LOW)
+		# GPIO.output(self.IN4,GPIO.HIGH)
+		self.setMotor(-self.speed[0], -self.speed[1])
+		self.moving = True
+  		# time.sleep(seconds)
+		# self.stop()
+
+	def right(self, seconds = 0):
+		if(self.moving):
+			return
+		print("im moving right")
+		# GPIO.output(self.IN1,GPIO.HIGH)
+		# GPIO.output(self.IN2,GPIO.LOW)
+		# GPIO.output(self.IN3,GPIO.LOW)
+		# GPIO.output(self.IN4,GPIO.LOW)
+		self.setMotor(self.speed[0], self.speed[1])
+		self.moving = True
+		# time.sleep(seconds)
+		# self.stop()
+		
+	def setPWMA(self,value):
+		self.PWMA.ChangeDutyCycle(value)
+
+	def setPWMB(self,value):
+		self.PWMB.ChangeDutyCycle(value)	
+		
+	def setMotor(self, left, right):
+		if((right >= 0) and (right <= 100)):
+			GPIO.output(self.IN1,GPIO.HIGH)
+			GPIO.output(self.IN2,GPIO.LOW)
+			self.PWMA.ChangeDutyCycle(right)
+		elif((right < 0) and (right >= -100)):
+			GPIO.output(self.IN1,GPIO.LOW)
+			GPIO.output(self.IN2,GPIO.HIGH)
+			self.PWMA.ChangeDutyCycle(0 - right)
+		if((left >= 0) and (left <= 100)):
+			GPIO.output(self.IN3,GPIO.HIGH)
+			GPIO.output(self.IN4,GPIO.LOW)
+			self.PWMB.ChangeDutyCycle(left)
+		elif((left < 0) and (left >= -100)):
+			GPIO.output(self.IN3,GPIO.LOW)
+			GPIO.output(self.IN4,GPIO.HIGH)
+			self.PWMB.ChangeDutyCycle(0 - left)
+	
+	def changeSpeed(self, speed):
+		self.speed[0] += speed 
+		self.speed[1] += speed
+  
+		self.speed[0] = max(0, min(100, self.speed[0]))
+		self.speed[1] = max(0, min(100, self.speed[1]))
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import sqlite3
 import AlphaBot
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_login import(
     LoginManager, UserMixin,
     login_user, login_required,
-    logout_user, current_uesr
+    logout_user, current_user
 )
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = "SKIBIDI67"
@@ -13,6 +14,70 @@ app.secret_key = "SKIBIDI67"
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+
+def requestUsersFromDB(dbName):
+    con = sqlite3.connect(dbName)
+
+    cur = con.cursor() #oggetto che opera sul db
+    #res risultato della query
+    res = cur.execute("SELECT * FROM Users") # fa le query
+    data = res.fetchall()
+    con.close()
+    print(data)
+    users = {}
+    for d in data:
+        users[d[1]] = {"password": d[2]} 
+
+    return users
+
+
+USERS = requestUsersFromDB("robot.db")
+print(USERS)
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+
+
+# USERS = {
+#     "admin": {"password": "alphabot"}
+# }
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id in USERS:
+        return User(user_id)
+    return None
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        hashed = hashlib.sha256(password.encode())
+        hashed = hashed.hexdigest()
+
+        # print("HARDDDDDDDDDDDDDDDDDDD:" + hashed)
+        #query
+        if username in USERS and USERS[username]["password"] == hashed:
+            login_user(User(username))
+            return redirect(url_for("control"))
+        
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+@app.route("/control")
+@login_required
+def control():
+    return render_template("control.html")
 
 def requestDB(dbName, act):
     con = sqlite3.connect(dbName)
@@ -28,8 +93,18 @@ def requestDB(dbName, act):
 
 actions = ["w", "a", "s", "d", "q", "e", "stop", "r"]
 
-ab = AlphaBot.AlphaBot()
+# chill = True
+# 
+# ab = None
+
+# if(chill):
+    
+ab = AlphaBot.ChillBot()
+# else:
+# ab = AlphaBot.AlphaBot()
+
 act = "stop"
+state = ""
 
 @app.route("/api/v1/sensors", methods = ['GET'])
 def getSensors():
@@ -41,34 +116,41 @@ def getSensors():
 
 @app.route("/api/v1/stopMotors")
 def stopMotors():
+    global state
     act = "stop"
     actt = requestDB("robot.db", act)
     getattr(ab, actt[0])()  
-    # state = act
-    return render_template("index.html")
+    state = act
+    return render_template("control.html")
 
 
 @app.route("/move")
 def move():
+    global state
     data = request.args.get("data")
     print("DAAAAAAAAAAAAAAAAAAAAAAAAAAA: " + data)
     act = "stop"
-    if(data):
-        if data == 'w':
-            print("wwwwwwwwwwwwwwwwwww")
-            act = "w"
-        elif data == 's':
-            print("ssssssssssssssssssss")
-            act = "s"
-        elif data == 'a':
-            print("aaaaaaaaaaaaaaaaaaaa")
-            act = "a"
-        elif data == 'd':
-            print("dddddddddddddddddddd")
-            act = "d"
-        elif data == 'stop':
-            print("STOPPPPPPPPPPPP")
-            act = "stop"
+    if data:
+        if data == state:
+            return render_template("control.html")
+        if data in actions:
+            print(data + data + data + data + data + data + data + data + data + data + data)
+            act = data
+        # if data == 'w':
+        #     print("wwwwwwwwwwwwwwwwwww")
+        #     act = "w"
+        # elif data == 's':
+        #     print("ssssssssssssssssssss")
+        #     act = "s"
+        # elif data == 'a':
+        #     print("aaaaaaaaaaaaaaaaaaaa")
+        #     act = "a"
+        # elif data == 'd':
+        #     print("dddddddddddddddddddd")
+        #     act = "d"
+        # elif data == 'stop':
+        #     print("STOPPPPPPPPPPPP")
+        #     act = "stop"
         else:
             print("Unknown")
     
@@ -80,12 +162,8 @@ def move():
             getattr(ab, nact)()  
         else:
             getattr(ab,nact)(float(params))
-    # state = act
-    return render_template("index.html")
-
-@app.route("/")
-def index():
-    return render_template("index.html")
+    state = act
+    return render_template("control.html")
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
